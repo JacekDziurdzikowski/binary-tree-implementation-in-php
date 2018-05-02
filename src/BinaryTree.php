dbbbff9f1ee77dd6ca55c30794bd08d4c18df761
Jacek Dziurdzikowski <jacek.dziurdzikowski@gmail.com>
2018-05-02 21:10:25 +0800

    added testing_binary_tree_insertion.php

--- src/BinaryTree.php
+++ src/BinaryTree.php
@@ -2,11 +2,49 @@
 
 class BinaryTree
 {
+    /**
+     * @var $value mixed
+     */
+    public $value;
+    /**
+     * @var $leftChild BinaryTree
+     */
+    public $leftChild;
+    /**
+     * @var $rightChild BinaryTree
+     */
+    public $rightChild;
+
+
     public function __construct($value)
     {
         $this->value = $value;
         $this->leftChild = null;
         $this->rightChild = null;
+    }
+
+
+    public function insertLeftChild($value)
+    {
+        if ($this->leftChild == null) {
+            $this->leftChild = new BinaryTree($value);
+        } else {
+            $newNode = new BinaryTree($value);
+            $newNode->leftChild = $this->leftChild;
+            $this->leftChild = $newNode;
+        }
+    }
+
+
+    public function insertRightChild($value)
+    {
+        if ($this->rightChild == null) {
+            $this->rightChild = new BinaryTree($value);
+        } else {
+            $newNode = new BinaryTree($value);
+            $newNode->rightChild = $this->rightChild;
+            $this->rightChild = $newNode;
+        }
     }
 
 
---
+++ src/testing_binary_tree_insertion.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+$a_node = new BinaryTree('a');
+$a_node->insertLeftChild('b');
+$a_node->insertRightChild('c');
+
+$b_node = $a_node->leftChild;
+$b_node->insertRightChild('d');
+
+$c_node = $a_node->rightChild;
+$c_node->insertLeftChild('e');
+$c_node->insertRightChild('f');
+
+$d_node = $b_node->rightChild;
+$e_node = $c_node->leftChild;
+$f_node = $c_node->rightChild;
+
+echo ($a_node->value ?? 'null') . PHP_EOL; #a
+echo ($b_node->value ?? 'null') . PHP_EOL; #b
+echo ($c_node->value ?? 'null') . PHP_EOL; #c
+echo ($d_node->value ?? 'null') . PHP_EOL; #d
+echo ($e_node->value ?? 'null') . PHP_EOL; #e
+echo ($f_node->value ?? 'null') . PHP_EOL; #f
