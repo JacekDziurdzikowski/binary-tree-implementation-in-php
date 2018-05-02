eb34ce52b39bfef479a22cec876b866f12988e6c
Jacek Dziurdzikowski <jacek.dziurdzikowski@gmail.com>
2018-05-02 19:36:52 +0800

    Basic project directory structure created

---
+++ src/BinaryTree.php
@@ -0,0 +1,14 @@
+<?php
+namespace jdz\BinaryTree;
+
+class BinaryTree
+{
+    public function __construct($value)
+    {
+        $this->value = $value;
+        $this->leftChild = null;
+        $this->rightChild = null;
+    }
+
+
+}
